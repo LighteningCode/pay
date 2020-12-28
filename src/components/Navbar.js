fd91ef61e8942256fe5dce3d085e7c2df294fb07
Elvis Agbesi <elvisagbesi@gmail.com>
2020-12-28 18:50:46 +0000

    feat: :construction: Navbar built

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,28 +1,31 @@
 import React from 'react'
 import { InlineIcon } from "@iconify/react";
 import searchIcon from '@iconify-icons/bi/search';
+import bellIcon from '@iconify-icons/bi/bell';
 
 function Navbar() {
     return (
         <nav className="flex flex-row">
             <section className="mr-auto flex flex-row">
-                <figure>
+                <figure className="mr-8">
                     <p>Pay</p>
                 </figure>
 
-                <div className="bg-white rounded-2xl px-4 pb-1 pt-1  text-gray-400 flex flex-row">
+                <div className="bg-white rounded-2xl px-4 pb-1 pt-1 focus:outline-none text-gray-400 flex flex-row">
                     <InlineIcon className="self-center" icon={searchIcon} />
                     <input placeholder="Search" className="text-sm pl-2" value={null} />
                 </div>
             </section>
 
-            <section>
-                <select>
+            <section className="flex flex-row">
+                <select className="text-sm bg-transparent self-center mr-1">
                     <option>EN</option>
                 </select>
-
-                <figure>
-                    <img src="" width="40" />
+                <div className="flex flex-row self-center mr-10">
+                    <InlineIcon icon={bellIcon} />
+                </div>
+                <figure className="self-center">
+                    <img src="./me.jpg" className="rounded-full" width="30" />
                 </figure>
             </section>
 
