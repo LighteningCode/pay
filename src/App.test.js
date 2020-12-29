98a78cecb6b0b8bfb6590cb134ed835e7f2652bb
Elvis Agbesi <elvisagbesi@gmail.com>
2020-12-29 01:02:18 +0000

    feat: :art: Done

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+describe("App", () => {
+    it("Should not test", () => {
 
-});
+    })
+})
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -8,17 +8,17 @@ function Navbar() {
         <nav className="flex flex-row pl-5">
             <section className="mr-auto flex flex-row">
                 <figure className="mr-8">
-                    <p className="font-extrabold italic text-2xl">Pay</p>
+                    <p className="font-extrabold  text-2xl">Pay</p>
                 </figure>
 
                 <div className="bg-white rounded-2xl px-4 pb-1 pt-1 focus:outline-none text-gray-400 flex flex-row">
-                    <InlineIcon className="self-center" icon={searchIcon} />
-                    <input placeholder="Search" className="text-sm pl-2" value={null} />
+                    <InlineIcon className="self-center text-gray-700" icon={searchIcon} />
+                    <input style={{fontSize:"10px"}} placeholder="Search" className="text-sm pl-2 w-44" value={null} />
                 </div>
             </section>
 
             <section className="flex flex-row">
-                <select className="text-sm bg-transparent self-center mr-1">
+                <select style={{fontSize:"10px"}} className="text-sm bg-transparent self-center mr-1">
                     <option>EN</option>
                 </select>
                 <div className="flex flex-row self-center mr-10">
